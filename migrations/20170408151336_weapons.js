9c79c74a88b7603f5581431a6e3513351812de83
joellegg <joellegg@bellsouth.net>
2017-04-08 15:16:11 -0500

    added weapons table

--- migrations/20170408151336_weapons.js
+++ migrations/20170408151336_weapons.js
@@ -1,8 +1,10 @@
 
-exports.up = function(knex, Promise) {
-  
-};
+exports.up = (knex, Promise) => (
+  knex.schema.createTable('weapons', table => {
+    table.increments() // same as 'id' int unsigned not null auto_increment
+    table.string('name').notNullable().unique()
+    table.integer('damage').notNullable()
+  })
+);
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = (knex, Promise) => knex.schema.dropTable('weapons')
